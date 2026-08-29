696e6f085edce6c5788bf5a91cee86ad5ad3d88a
Abdelkarim Aridj <abdelkarim.ardj@gmail.com>
2026-08-29 23:04:15 +0100

    added routing via react router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
-
+import { Layout, Settings } from "lucide-react"
+import { Toaster } from "react-hot-toast"
+import { Navigate, Route, Routes } from "react-router-dom"
+import Dashboard from "./pages/Dashboard"
+import Employees from "./pages/Employees"
+import Attendance from "./pages/Attendance"
+import Leave from "./pages/Leave"
+import Payslips from "./pages/Payslips"
+import LoginLanding from "./pages/LoginLanding"
+import PrintPayslip from "./pages/PrintPayslip"
 export const App = () => {
   return (
-    <div>App</div>
+    <>
+    <Toaster />
+    <Routes>
+      <Route path="/login" element={ <LoginLanding/>}/>
+      <Route element={<Layout />}>
+        <Route path="/dashboard" element={<Dashboard/>}/>
+        <Route path="/employees" element={<Employees/>}/>
+        <Route path="/attendance" element={<Attendance/>}/>
+        <Route path="/leave" element={<Leave/>}/>
+        <Route path="/payslips" element={<Payslips/>}/>
+        <Route path="/settings" element={<Settings/>}/>
+      </Route>
+      <Route path="/print/payslips/:id" element={ <PrintPayslip/>}/>
+
+      <Route path="*" element={<Navigate to="/dashboard" replace/>}/>
+    </Routes>
+
+    </>
   )
 }
 
